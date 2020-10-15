dcc30d48fee0ff9cfbe8043f0e59bcf1b98b0228
Ilya Istomin <ilyaistomin.888@yandex.ru>
2020-10-15 12:41:02 +0300

    Initial commit

---
+++ students_base/src/test/java/by/belstu/istomin/students_base/StudentsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belstu.istomin.students_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
